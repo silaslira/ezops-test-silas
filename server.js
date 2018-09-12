7a592777d28128c7bfe592c683ce83a2ba3416b8
Silas Lira <silas.lira@ymail.com>
2018-09-12 17:01:05 -0300

    Connection to DB

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+var dbUrl = 'mongodb://messages:M2r4T7aA3G2@ds255332.mlab.com:55332/messages';
+
+var Message = mongoose.model('Message', { name : String, message : String});
+
+mongoose.connect(dbUrl , (err) => {
+    console.log('mongodb connected', err);
+});
 
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
